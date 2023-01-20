7320c7142b10dba7fe3bf12ec1398f1541dbd82d
Gohar Ali Siddiqui <gohar.siddiqui@tum.de>
2023-01-20 22:55:43 +0500

    Added notification to clients for PART

--- src/Commander/CommanderPART.cpp
+++ src/Commander/CommanderPART.cpp
@@ -2,6 +2,11 @@
 
 namespace AFG
 {
+    void cPART_reponseNotifyChannelOfPart(Client &caller, Channel &channel) // Notify channel of the quited user
+    {
+        channel.announce(":" + caller.get_nick() + "!" + caller.get_username() + "@" + caller.get_hostname() + " PART " + channel.getName() + " : " + MSG_END_SEQ);
+    }
+
     void Commander::commandPART(Client &caller, std::vector<std::string> &channel_names)
     {
         if (channel_names.size() < 1)
@@ -21,7 +26,10 @@ namespace AFG
                     if (it->getName() == channel_names[i])
                     {
                         if (it->hasUser(caller))
+                        {
+                            cPART_reponseNotifyChannelOfPart(caller, *it);
                             it->removeUser(caller);
+                        }
                         else
                             caller.respond(":AFGchat 442 NOTICE Auth : " + channel_names[i] + " :You're not on that channel\n");
                     }
