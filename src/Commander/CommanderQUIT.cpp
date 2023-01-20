2dc48820f4cf60dbafdee9136d2ac8e305cb8639
Gohar Ali Siddiqui <gohar.siddiqui@tum.de>
2023-01-20 22:36:52 +0500

    Added notification to the channel that a user has quit

--- src/Commander/CommanderQUIT.cpp
+++ src/Commander/CommanderQUIT.cpp
@@ -2,6 +2,12 @@
 
 namespace AFG
 {
+
+    void cQUIT_reponseNotifyChannelOfQuit(Client &caller, Channel &channel) // Notify channel of the quited user
+    {
+        channel.announce(":" + caller.get_nick() + "!" + caller.get_username() + "@" + caller.get_hostname() + " QUIT :" + " " + MSG_END_SEQ);
+    }
+
     void Commander::commandQUIT(Client &caller)
     {
         std::list<AFG::Channel>::iterator it;
@@ -12,6 +18,8 @@ namespace AFG
                 it->removeUser(caller);
                 if (it->isOperator(caller))
                     it->removeOperator(caller);
+                cQUIT_reponseNotifyChannelOfQuit(caller, *it);
+                break;
             }
         }
         close(caller.get_fd());
