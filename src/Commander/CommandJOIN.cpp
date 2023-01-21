038263b6806f0c3b1139bde4acdc98d6060839c6
Fredi-B <fredibechtoldt@outlook.com>
2023-01-21 11:38:39 +0100

    fixed typo

--- src/Commander/CommandJOIN.cpp
+++ src/Commander/CommandJOIN.cpp
@@ -24,7 +24,7 @@ namespace AFG
         if (channel.getTopic() == "")
             caller.respond(":" SERVER_NAME " " RPL_NOTOPIC " " + caller.get_nick() + " " + channel.getName() + " :No topic is set" + MSG_END_SEQ);
         else
-            caller.respond(":" SERVER_NAME " " RPL_NOTOPIC " " + caller.get_nick() + " " + channel.getName() + " :" + channel.getTopic() + MSG_END_SEQ);
+            caller.respond(":" SERVER_NAME " " RPL_TOPIC " " + caller.get_nick() + " " + channel.getName() + " :" + channel.getTopic() + MSG_END_SEQ);
     }
 
     void cJOIN_reponseSendNewcomerNamelist(Client &caller, Channel &channel) // Notify channel of the joined user
